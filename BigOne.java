6f50f827d988fe9c1c28b06b2a16d5c91928065b
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:27:38 +0200

    Create BigOne.java

---
+++ BigOne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Напишете програма, която прочита 2 числа със запетая от конзолата и печата кое е по-голямото.
+//Отпечатаният резултат трябва да съдържа само числото без никакви допълнителни пояснения.
+//Например: “резултатът е 10” e невалидно.
+
+public class BigOne {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double firstNumber = scanner.nextDouble();
+        double secondNumber = scanner.nextDouble();
+
+        if (firstNumber > secondNumber){
+            System.out.print(firstNumber);
+        }
+        else {
+            System.out.println(secondNumber);
+        }
+    }
+}
